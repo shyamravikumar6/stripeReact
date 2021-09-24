9f784b5b5e2240a094f639ee3e3b14c07ad1758b
shyamravikumar6 <shyamravikumar6@gmail.com>
2021-09-24 16:58:06 +0530

    new changes

--- src/PaymentStatus.js
+++ src/PaymentStatus.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {Card,CardTitle} from 'reactstrap';
 function PaymentStatus({status}) {
     return (
-        <Card className={`${status.success?'bg-green':'bg-red'}`} >
+        <Card className={`${status.success?'bg-success':'bg-danger'}`} >
          <CardTitle > `{status.success? 'Payment Done successfully':'payemnt failed '}</CardTitle>
-         <button><a href="http://localhost:3000/testing">Go back</a></button>
+         <button><a href="https://stripecheckout-mu.vercel.app">Go back</a></button>
         </Card>
     )
 }
